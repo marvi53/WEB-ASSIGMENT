c4689bdca0c9c17097e7e2855ffe018be1a2ce0f
marvi53 <imrealmarvi@gmail.com>
2021-12-28 13:44:43 +0500

    my initial commit

---
+++ partials/_nav.php
@@ -0,0 +1,22 @@
+<nav class="d-flex justify-content-center navbar navbar-expand-lg  navbar-secondary bg-dark >
+  <a class="navbar-brand" href="/assigmentLog"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="nav nav-tabs nav-justified" id="navbarSupportedContent">
+    <ul  class="nav nav-pills nav-justified" role="tablist">
+      <li class="nav-item ">
+        <a class="nav-link" href="/assigmentLog/home.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/assigmentLog/login.php">Login</a>
+      </li>
+      <li class="nav-item" >
+        <a class="nav-link" href="/assigmentLog/signUp.php">Signup</a>
+      </li>
+      
+    </ul>
+    
+  </div>
+</nav>
---
+++ partials/_navHome.php
@@ -0,0 +1,21 @@
+<nav class="d-flex justify-content-around navbar navbar-expand-lg  navbar-white bg-secondary >
+  <a class="navbar-brand" href="/assigmentLog"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="btn-group btn-group-lg bg-dark  border border-dark rounded-pill nav nav-tabs nav-justified "style="padding:10px" id="navbarSupportedContent">
+    <ul  class="nav nav-pills nav-justified" role="tablist">
+      <li class="nav-item ">
+        <a class="nav-link" href="/assigmentLog/home.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/assigmentLog/post.php">Post</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/assigmentLog/todo.php">TODO</a>
+      </li>
+    </ul>
+    
+  </div>
+</nav>
